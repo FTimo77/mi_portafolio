bcdc555e5c6f4f0ae77c187eaac4cd3af35e75b3
FTimo77 <2005maldonado@gmail.com>
2025-06-06 19:05:15 -0500

    Otra corrección

--- build.sh
+++ build.sh
@@ -5,6 +5,17 @@ pip install -r requirements.txt
 rm -rf public
 reflex init
 reflex export --frontend-only
-unzip frontend.zip -d public
-rm -f frontend.zip
+
+# Verifica si el archivo existe y descomprime correctamente
+if [ -f frontend.zip ]; then
+    unzip frontend.zip -d public
+    rm -f frontend.zip
+elif [ -f web.zip ]; then
+    unzip web.zip -d public
+    rm -f web.zip
+else
+    echo "No se encontró el archivo zip exportado por Reflex"
+    exit 1
+fi
+
 deactivate
