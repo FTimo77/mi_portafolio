77f44f5f08eeb7f2d1c09c57fa30a182852fbcf9
FTimo77 <2005maldonado@gmail.com>
2025-06-08 16:26:37 -0500

    correción

--- build.sh
+++ build.sh
@@ -5,21 +5,6 @@ pip install -r requirements.txt
 rm -rf public
 reflex init
 reflex export --frontend-only
-
-if [ -f frontend.zip ]; then
-    unzip -j frontend.zip -d public
-    rm -f frontend.zip
-elif [ -f web.zip ]; then
-    unzip -j web.zip -d public
-    rm -f web.zip
-else
-    echo "No se encontró el archivo zip exportado por Reflex"
-    exit 1
-fi
-
-echo "Contenido del directorio actual:"
-ls -l
-echo "Contenido de public (si existe):"
-ls -l public || echo "No existe la carpeta public"
-
+unzip frontend.zip -d public
+rm -f frontend.zip
 deactivate
