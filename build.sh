59dcc133f32f4955f3b89206154da7ba1f1ae3d8
Timothy Maldonado <2005maldonado@gmail.com>
2025-06-08 18:37:14 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,20 @@
+#!/bin/bash
+
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+
 rm -rf public
 reflex init
 reflex export --frontend-only
+
 unzip frontend.zip -d public
+
+# 👇 Esta línea es esencial para que el gif se suba
+cp code.gif public/
+
 rm -f frontend.zip
 deactivate
 
-# Copiar el gif al folder public para que sea accesible
-cp assets/code.gif public/code.gif
 
-rm -f frontend.zip
